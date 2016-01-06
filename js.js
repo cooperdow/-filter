60556577d73244720264bd383dc97712f9a1e92c
cooperdow <cooperdow@users.noreply.github.com>
2016-01-05 22:10:35 -0700

    Update js.js
    changed words

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-angular.module('filterExample', [])
-.controller('MainCtrl', function($scope, $filter) {
-  $scope.originalText = 'hello';
-  $scope.filteredText = $filter('uppercase')($scope.originalText);
+angular.module('html.html', [])
+.controller('MainController', function($scope, $filter) {
+  $scope.plainPhrase = 'HELLO, TEST';
+  $scope.filterPhrase = $filter('lowercase')($scope.plainPhrase);
 });
